ec82524e718abfe58a6eee646a514483c5b6acee
Johannes M. Schmitt <schmittjoh@gmail.com>
2013-01-14 17:39:22 +0100

    adds YamlUtils

---
+++ src/Scrutinizer/Util/YamlUtils.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Scrutinizer\Util;
+
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Yaml\Yaml;
+
+class YamlUtils
+{
+    /**
+     * Safely parses the input string.
+     *
+     * In contrast to the original Yaml class, we do not try to resolve the input to a filename first. Consequentially,
+     * there is no need to sanitize the input prior to calling this method.
+     *
+     * @param string $input
+     *
+     * @return mixed
+     */
+    public static function safeParse($input)
+    {
+        Yaml::disablePhpParsing();
+        Yaml::disableObjectSupport();
+
+        return (new Parser())->parse($input);
+    }
+}

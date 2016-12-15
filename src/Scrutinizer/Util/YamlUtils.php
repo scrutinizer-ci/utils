f8aa67439e0545adcf61c5cf3dff8d8453229ecb
linglongmeng <linglong.meng@scrutinizer-ci.com>
2016-12-15 16:46:21 +0100

    add validator for duplicated keys

--- src/Scrutinizer/Util/YamlUtils.php
+++ src/Scrutinizer/Util/YamlUtils.php
@@ -2,6 +2,7 @@
 
 namespace Scrutinizer\Util;
 
+use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Parser;
 use Symfony\Component\Yaml\Yaml;
 
@@ -23,6 +24,56 @@ class YamlUtils
             Yaml::setPhpParsing(false);
         }
 
+        if (!self::validateDuplicatedKey($input)){
+            throw new ParseException("Duplicate key detected while parsing YAML :{".$input."}");
+        }
+
         return (new Parser())->parse($input, true, false);
+    }
+
+    /**
+     * Validate if there is duplicated keys in the input yml string
+     *
+     * @param string $input
+     * @return bool
+     */
+    private static function validateDuplicatedKey($input) {
+
+        $lines = explode("\n", $input);
+        $data = array();
+        $indentationofLastline = 0;
+        
+        foreach ($lines as $linenumber => $line) {
+            $indentationofCurrentLine = strlen($line) - strlen(ltrim($line, ' '));
+            $value=trim($line);
+            if (false !== strpos($value, ':')) {
+                $key = substr($value, 0, strpos($value, ":"));
+            }  else {
+                $key = $value;
+            }
+
+            if ($indentationofCurrentLine > $indentationofLastline) {
+                $data[$indentationofCurrentLine][$key] = array();
+            } elseif ($indentationofCurrentLine === $indentationofLastline) {
+                if (false === strpos($key, '-') && isset($data[$indentationofCurrentLine][$key])) {
+                    return false;
+                } else {
+                    $data[$indentationofCurrentLine][$key] = array();
+                }
+            } else {
+                foreach ($data as $indentation => $keys) {
+                    if ($indentation > $indentationofCurrentLine) {
+                        unset($data[$indentation]);
+                    }
+                }
+                if (false === strpos($key, '-') && isset($data[$indentationofCurrentLine][$key])) {
+                    return false;
+                } else {
+                    $data[$indentationofCurrentLine][$key] = array();
+                }
+            }
+            $indentationofLastline = $indentationofCurrentLine;
+        }
+        return true;
     }
 }
--- tests/Scrutinizer/Tests/Util/YamlUtilsTest.php
+++ tests/Scrutinizer/Tests/Util/YamlUtilsTest.php
@@ -3,11 +3,107 @@
 namespace Scrutinizer\Tests\Util;
 
 use Scrutinizer\Util\YamlUtils;
+use Symfony\Component\Yaml\Exception\ParseException;
 
 class YamlUtilsTest extends \PHPUnit_Framework_TestCase
 {
     public function testSafeParseDoesNotResolveFilename()
     {
         $this->assertEquals(__FILE__, YamlUtils::safeParse(__FILE__));
+    }
+
+
+    public function testDuplicatedKeyInGeneral()
+    {
+        $input = <<<YAML
+build:
+    test:
+        override:
+            - test
+build:
+    environment: true
+YAML;
+
+        $this->setExpectedException(ParseException::class);
+        $actualArray = YamlUtils::safeParse($input);
+
+    }
+
+    public function testDuplicatedKeyInChildNode()
+    {
+        $input = <<<YAML
+build:
+    test:
+        override:
+            - test
+    test:
+        before:
+            - test
+YAML;
+
+        $this->setExpectedException(ParseException::class);
+        $actualArray = YamlUtils::safeParse($input);
+    }
+
+    public function testDuplicatedContentSequencesShouldNotThrowException()
+    {
+        $input = <<<YAML
+build:
+    test:
+        override:
+            - test
+            - test
+        
+YAML;
+
+        $actualArray = YamlUtils::safeParse($input);
+        $expectedArray = array(
+            'build' => array(
+                'test' => array(
+                    'override' => array(
+                        'test', 'test'
+                    )
+                )
+            )
+        );
+
+        $this->assertEquals($expectedArray, $actualArray);
+    }
+
+    public function testSequencesWithChildrenArray()
+    {
+        $input = <<<YAML
+build:
+    test:
+        override:
+            - 
+                command: 
+                    analysis: true
+            - 
+                command: 
+                    analysis: true
+YAML;
+
+        $actualArray = YamlUtils::safeParse($input);
+        $expectedArray = array(
+            'build' => array(
+                'test' => array(
+                    'override' => array(
+                        array(
+                            'command' => array(
+                                'analysis' => true
+                            )
+                        ),
+                        array(
+                            'command' => array(
+                                'analysis' => true
+                            )
+                        )
+                    )
+                )
+            )
+        );
+
+        $this->assertEquals($expectedArray, $actualArray);
     }
 }
